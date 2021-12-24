690d9d9dc48fe4cbc114ebed25f1cbfe1894b706
zzy19970701 <83382898+zzy19970701@users.noreply.github.com>
2021-12-24 10:34:52 +0800

    Add files via upload

---
+++ plumber.R
@@ -0,0 +1,34 @@
+#
+# This is a Plumber API. You can run the API by clicking
+# the 'Run API' button above.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+library(plumber)
+
+#* @apiTitle Plumber Example API
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @post /echo
+function(msg) {
+    list(paste0("The message is: '", msg, "'"))
+}
+
+#* Plot a histogram
+#* @serializer png
+#* @get /plot
+function() {
+    rand <- rnorm(100)
+    hist(rand)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @post /sum
+function(a,b) {
+   as.numeric(a)+ as.numeric(b)
+}
